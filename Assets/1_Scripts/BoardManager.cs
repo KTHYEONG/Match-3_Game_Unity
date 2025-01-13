afbb25553c03db5de7f83fbb5b60d5a381941824
KimTaeHyeong <xogud3624@gmail.com>
2025-01-13 22:26:08 +0900

    Board 초기 배치 구현

--- Assets/1_Scripts/BoardManager.cs
+++ Assets/1_Scripts/BoardManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BoardManager
+public class BoardManager : MonoBehaviour
 {
     public static BoardManager mInstance = null;
     public static BoardManager instance
@@ -15,8 +15,23 @@ public class BoardManager
         }
     }
 
+    [SerializeField] private int boardWidth = 8;
+    [SerializeField] private int boardHeight = 8;
+    [SerializeField] private GameObject[] fruitPrefabs;
+
+    private GameObject[][] board;
+
     public void InitializeBoard()
     {
-
+        for (int i = 0; i < boardHeight; i++)
+        {
+            for (int j = 0; j < boardWidth; j++)
+            {
+                int randomIdx = Random.Range(0, fruitPrefabs.Length);
+                Vector2 pos = new Vector2(i, j);
+                GameObject fruit = Instantiate(fruitPrefabs[randomIdx], pos, Quaternion.identity);
+                board[i][j] = fruit;
+            }
+        }
     }
 }
