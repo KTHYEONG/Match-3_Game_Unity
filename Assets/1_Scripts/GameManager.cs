542a4dac6cc90585598cbcf705c5757bf69d7de4
KimTaeHyeong <xogud3624@gmail.com>
2025-01-13 18:30:53 +0900

    GameManger 초기 구현
    GameManger 스크립트 추가(싱글톤) 및 gameobject 추가

--- Assets/1_Scripts/GameManager.cs
+++ Assets/1_Scripts/GameManager.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
